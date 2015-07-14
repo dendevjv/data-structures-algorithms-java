5c5ba443c9ce812182d3dd78552c7875ff27311e
dendevjv <dendevjv@yandex.ru>
2015-07-14 23:31:55 +0400

    Ch6 Found solution to Knapsack Problem

---
+++ src/dsalgjava/ch06/knapsack/Sack.java
@@ -0,0 +1,77 @@
+package dsalgjava.ch06.knapsack;
+
+public class Sack {
+    private int[] items;
+    private int size;
+    
+    Sack(int maxSize) {
+        items = new int[maxSize];
+        size = 0;
+    }
+    
+    void add(int item) {
+        items[size++] = item;
+    }
+    
+    int remove() {
+        return items[--size];
+    }
+    
+    int getTotal() {
+        int total = 0;
+        for (int i : items) {
+            total += i;
+        }
+        return total;
+    }
+    
+    public boolean fill(int[] sourceArr, int targetSum) {
+        for (int j = 0; j < sourceArr.length; j++) {
+            if (fill2(j, sourceArr, targetSum)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean fill2(int i, int[] sourceArr, int t) {
+        while (i < sourceArr.length && sourceArr[i] > t) {
+            i++;
+        }
+        if (i >= sourceArr.length) {
+            return false;
+        }
+        add(sourceArr[i]);
+        t -= sourceArr[i];
+        if (t == 0) {
+            return true;
+        }
+        for (int j = i + 1; j < sourceArr.length; j++) {
+            if (fill2(j, sourceArr, t)) {
+                return true;
+            }
+        }
+        remove();
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (int j = 0; j < size; j++) {
+            if (j > 0) {
+                sb.append(", ");
+            }
+            sb.append(items[j]);
+        }
+        sb.append(";  total=");
+        sb.append(getTotal());
+        sb.append(']');
+        return sb.toString();
+    }
+    
+    void display() {
+        System.out.println(toString());
+    }
+}
---
+++ src/dsalgjava/ch06/knapsack/SackApp.java
@@ -0,0 +1,26 @@
+package dsalgjava.ch06.knapsack;
+
+import java.util.Scanner;
+
+public class SackApp {
+    private static int[] items = { 11, 8, 7, 6, 5 };
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter target sum: ");
+            int t = in.nextInt();
+            if (t < 0) break;
+            Sack sack = new Sack(10);
+            boolean r = sack.fill(items, t);
+            if (r) {
+                System.out.println("Sack filled:");
+                sack.display();
+            } else {
+                System.out.println("Can not fill the sack");
+            }
+        }
+        in.close();
+    }
+
+}
